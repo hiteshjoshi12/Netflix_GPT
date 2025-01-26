605620d5f2bfd9f8a8bec91a4ce9a3f9d3e32db7
Hitesh joshi <joshihitesh29@gmail.com>
2025-01-26 10:40:33 +0530

    Add constants for logo, banner, API options, and supported languages

---
+++ src/Utils/Constants.jsx
@@ -0,0 +1,21 @@
+export const logo =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const banner =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/df6621a3-890c-4ca0-b698-90bd5152f3d1/20a59be7-7062-4991-bca0-805e9a7f2716/IN-en-20240107-trifectadaily-perspective_alpha_website_large.jpg";
+
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTUxZGY1M2VjNTRlMjA0MTZjYzA0ZjAxNDllNzEzMyIsInN1YiI6IjY0YjVmYTlmNzg1NzBlMDBjNmQ1YjgzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VwDWfq5J3wPkUk797oM3kbALh4g-Z-_ZQrzdr2wUt70",
+  },
+};
+
+export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
